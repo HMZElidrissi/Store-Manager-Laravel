96839efa2a51e26cddcb273ba360f1e5a6465e65
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-02-08 15:55:26 +0100

    creating migrations

--- database/migrations/2024_12_08_000000_create_users_table.php
+++ database/migrations/2024_12_08_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role_id')->constrained('roles');
+            $table->string('rib')->nullable();
+            $table->string('address')->nullable();
+            $table->binary('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
